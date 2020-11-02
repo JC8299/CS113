f0bebc9579b7682999e79c1fd1ce0d8680934374
Justin Cheng <justincheng.cheng@gmail.com>
2020-11-02 12:40:47 -0800

    Added simple timer and GameManager
    Only in SampleScene. Will add to others when games are all done.

---
+++ CS113/Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance {get; private set;}
+
+    public int lifesMax;
+    public int lifesLeft;
+
+    public int minigamesCompleted;
+    public List<string> minigamesList;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+            lifesLeft = lifesMax;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //call when minigame is done
+    //boolean is for if they pass (true) or fail (false)
+    public void CurrentMinigameCompleted(bool success)
+    {
+        if (!success)
+        {
+            lifesLeft--;
+        }
+        minigamesCompleted++;
+
+        string nextMinigame = minigamesList[Random.Range(0, minigamesList.Count)];
+        while(nextMinigame == SceneManager.GetActiveScene().name)
+        {
+            nextMinigame = minigamesList[Random.Range(0, minigamesList.Count)];
+        }
+        SceneManager.LoadScene(nextMinigame);
+    }
+}
